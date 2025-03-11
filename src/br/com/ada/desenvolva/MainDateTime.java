d268c2b30821e04f21fb4ab6e06e4ebd68d9cd2f
William Cesar Santos <william_cesar_santos@hotmail.com>
2025-03-10 22:00:39 -0300

    Update

--- src/br/com/ada/desenvolva/MainDateTime.java
+++ src/br/com/ada/desenvolva/MainDateTime.java
@@ -1,12 +1,23 @@
 package br.com.ada.desenvolva;
 
 import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class MainDateTime {
 
     public static void main(String[] args) {
-        installments(120, 15);
+//        installments(120, 15);
+        LocalTime first = LocalTime.of(8, 30);
+        LocalTime second = LocalTime.of(12, 00);
+        // Calcular a quantidade de horas entre dois horários
+        // Dado que eu possua o horário de 08:00 e 12:00,
+        // desejo saber quantas horas se passaram
+//        first.until(second, ChronoUnit.HOURS);
+        var spentTime = Duration.between(first, second);
+        System.out.println("Já se passaram " + spentTime.toHours() + ":"
+                + spentTime.toMinutesPart() + " horas");
     }
 
     public static void installments(int amount, int dueDay) {
