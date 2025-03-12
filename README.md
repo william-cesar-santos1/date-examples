ddaca228112492d0ff47d4df8a9ee19ec13943cc
William Cesar Santos <william_cesar_santos@hotmail.com>
2025-03-12 19:38:43 -0300

    Update

---
+++ README.md
@@ -0,0 +1,8 @@
+Exercicios
+
+Salário - Calcule o salário semanal de um funcionário com base na quantidade 
+de horas que ele trabalhou. Lembre-se que cada dia pode ter um valor de hora diferente, logo, 
+o valor da hora deve ser solicitado ao usuário.
+
+Deve ser solicitado o dia trabalhado, horário de entrada e saída para 
+cada periódo e também o valor da hora no dia.
--- src/br/com/ada/desenvolva/MainDateTime.java
+++ src/br/com/ada/desenvolva/MainDateTime.java
@@ -1,23 +1,32 @@
 package br.com.ada.desenvolva;
 
 import java.time.DayOfWeek;
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainDateTime {
 
     public static void main(String[] args) {
 //        installments(120, 15);
-        LocalTime first = LocalTime.of(8, 30);
-        LocalTime second = LocalTime.of(12, 00);
+//        LocalTime first = LocalTime.of(8, 30);
+//        LocalTime second = LocalTime.of(12, 00);
         // Calcular a quantidade de horas entre dois horários
         // Dado que eu possua o horário de 08:00 e 12:00,
         // desejo saber quantas horas se passaram
 //        first.until(second, ChronoUnit.HOURS);
-        var spentTime = Duration.between(first, second);
-        System.out.println("Já se passaram " + spentTime.toHours() + ":"
-                + spentTime.toMinutesPart() + " horas");
+//        var spentTime = Duration.between(first, second);
+//        System.out.println("Já se passaram " + spentTime.toHours() + ":"
+//                + spentTime.toMinutesPart() + " horas");
+        // dd - day
+        // MM - month
+        // yy - year
+        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        var currentDate = LocalDateTime.now();
+        System.out.println(formatter.format(currentDate));
+
+        var dateAsString = "10/03/2025 01:00";
+        System.out.println(formatter.parse(dateAsString));
     }
 
     public static void installments(int amount, int dueDay) {
